8a8affbe23418caebca31c5953f1345125a19d97
AvinashReddy9 <reddiavinahg@gmail.com>
2019-12-24 15:17:36 -0800

    Created sample AutoComplete TextView Project

---
+++ AutoCompletetTextViewApp/app/src/main/java/com/example/autocompletettextviewapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.autocompletettextviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    AutoCompleteTextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] array = {"Google", "Facebook","LinkedIn", "Telsa", "Lucid", "Mediation"};
+        textView = (AutoCompleteTextView)findViewById(R.id.textView);
+        textView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,array));
+         textView.setThreshold(1);
+         textView.setTextColor(Color.RED);
+    }
+
+
+}
